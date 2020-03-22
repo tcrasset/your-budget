092991fabdea8cfd49656152b77b1e2d691bb394
tcrasset <tomcrasset@gmail.com>
2020-03-22 18:12:24 +0100

    Show more info for each transaction

--- lib/showTransactionsPage.dart
+++ lib/showTransactionsPage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mybudget/categories.dart';
 import 'package:mybudget/entries.dart';
-
 import 'package:mybudget/database_creator.dart';
+import 'package:mybudget/utils.dart';
 
 class ShowTransactionPage extends StatefulWidget {
-  String title;
+  final String title;
 
   ShowTransactionPage({@required this.title});
 
@@ -19,14 +20,47 @@ class ShowTransactionPageState extends State<ShowTransactionPage> {
 
   List<MoneyTransaction> moneyTransactionList;
 
-  Future<List<MoneyTransaction>> _getMoneyTransactions() async {
-    return SQLQueryClass.getMoneyTransactions();
-  }
-
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  
+  Container _buildTransactionContainer(AsyncSnapshot snapshot, int index) {
+    
+    MoneyTransaction moneyTransaction = snapshot.data[0][index];
+    List<SubCategory> subcategories = snapshot.data[3];
+
+    SubCategory subcategory = subcategories
+          .singleWhere((subcategory) => subcategory.id == moneyTransaction.subcatID,
+                        orElse: () => null);
+
+    String subcategoryName;
+    if(subcategory == null){
+      subcategoryName = "No subcategory";
+    }else{
+      subcategoryName = subcategory.name;
+    }
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(child: Text("MyMemo", textAlign: TextAlign.left,),),
+              Text("${moneyTransaction.amount} €", textAlign: TextAlign.right),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(subcategoryName, textAlign: TextAlign.left,),
+              Expanded(child: Text(getDateString(moneyTransaction.date), textAlign: TextAlign.right)),
+            ],
+          ),
+      ]));
   }
 
   @override
@@ -43,21 +77,29 @@ class ShowTransactionPageState extends State<ShowTransactionPage> {
           Expanded(
             child : FutureBuilder(
               // Perform async operations before displaying the interface
-              future: _getMoneyTransactions(),
+              future: Future.wait([
+                  SQLQueryClass.getMoneyTransactions(),
+                  SQLQueryClass.getPayees(),
+                  SQLQueryClass.getAccounts(),
+                  SQLQueryClass.getSubCategories(),
+              ]),
               builder: (context, snapshot) {
                 switch (snapshot.connectionState){
                   case ConnectionState.none:
                     return new Container();
+                  break;
                   case ConnectionState.waiting:
                     return new Center(child: new CircularProgressIndicator());
+                  break;
                   case ConnectionState.active:
                     return new Container();
-                  case ConnectionState.done:
+                  break;
+                  case ConnectionState.done: {
                     if (snapshot.hasError){
                       return new Text('${snapshot.error}',
                           style: TextStyle(color: Colors.red));
                     } else {
-                        moneyTransactionList = snapshot.data;
+                        moneyTransactionList = snapshot.data[0];
                         return new Container(
                           child: new ListView.separated(
                             shrinkWrap: true,
@@ -65,16 +107,17 @@ class ShowTransactionPageState extends State<ShowTransactionPage> {
                             separatorBuilder: (BuildContext context, int index) =>
                                 Divider(height: 1, color: Colors.black12),
                             itemBuilder: (BuildContext context, int index) {
-                              return new ListTile(
-                                title: Text('${moneyTransactionList[index].amount}'),
-                                onTap: () {
-                                  // Navigator.pop(context, item);
-                                }
-                              );
+                              return _buildTransactionContainer(snapshot, index);
                             },
                           )
                         );
                     }
+                  }
+                  break;
+                  default:
+                    return new Container();
+                  break;
+
                 }
               }
             )
