7bcb0c3741025c2d5b6e52617ce96c8a412d6cdd
tcrasset <tomcrasset@gmail.com>
2020-06-20 12:12:25 +0200

    Move showTransaction components to their own folder

---
+++ lib/screens/showTransactions/components/transaction.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mybudget/models/categories.dart';
+import 'package:mybudget/models/entries.dart';
+import 'package:mybudget/models/utils.dart';
+
+Widget transaction(AsyncSnapshot snapshot, int index) {
+  MoneyTransaction moneyTransaction = snapshot.data[0][index];
+  List<SubCategory> subcategories = snapshot.data[3];
+
+  SubCategory subcategory = subcategories.singleWhere(
+      (subcategory) => subcategory.id == moneyTransaction.subcatID,
+      orElse: () => null);
+
+  String subcategoryName;
+  if (subcategory == null) {
+    subcategoryName = "No subcategory";
+  } else {
+    subcategoryName = subcategory.name;
+  }
+
+  return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(children: [
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                "MyMemo",
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Text("${moneyTransaction.amount} €", textAlign: TextAlign.right),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Text(
+              subcategoryName,
+              textAlign: TextAlign.left,
+            ),
+            Expanded(child: Text(getDateString(moneyTransaction.date), textAlign: TextAlign.right)),
+          ],
+        ),
+      ]));
+}
