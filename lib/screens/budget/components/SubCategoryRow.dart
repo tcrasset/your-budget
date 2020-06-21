aac32908244ba381e23347b72d0a32607e49c47d
tcrasset <tomcrasset@gmail.com>
2020-06-21 19:33:38 +0200

    Refactr SubCategoryRow using WidgetView

--- lib/screens/budget/components/SubCategoryRow.dart
+++ lib/screens/budget/components/SubCategoryRow.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:mybudget/models/categories.dart';
 import 'package:flutter/services.dart';
-import 'package:mybudget/models/SQLQueries.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
-/// Widget containing and displaying the information a subcategory
+import 'package:mybudget/models/categories.dart';
+import 'package:mybudget/models/SQLQueries.dart';
+import 'package:mybudget/components/widgetViewClasses.dart';
+
+// Widget containing and displaying the information a subcategory
+
 class SubcategoryRow extends StatefulWidget {
   final SubCategory subcat;
   SubcategoryRow({Key key, @required this.subcat}) : super(key: key);
 
   @override
-  _SubcategoryRowState createState() => _SubcategoryRowState();
+  _SubcategoryRowController createState() => _SubcategoryRowController();
 }
 
-class _SubcategoryRowState extends State<SubcategoryRow> {
+class _SubcategoryRowController extends State<SubcategoryRow> {
   MoneyMaskedTextController _budgetedController;
   MoneyMaskedTextController _availableController;
   TextEditingController _nameController;
-
-  final TextStyle _subcategoryTextStyle = new TextStyle(color: Colors.black, fontSize: 16.0);
-  final TextStyle _greenNumberTextStyle = new TextStyle(color: Colors.green, fontSize: 16.0);
-  final TextStyle _redNumberTextStyle = new TextStyle(color: Colors.red, fontSize: 16.0);
 
   @override
   void initState() {
@@ -42,12 +41,52 @@ class _SubcategoryRowState extends State<SubcategoryRow> {
     super.dispose();
   }
 
-  //TODO : Update MainCategory budget when subcategory budget is changed
+  handleSubcategoryNameChange(String value) {
+    print("Changed name of subcategory from ${widget.subcat.name} to ${_nameController.text} ");
+    if (value != widget.subcat.name) {
+      setState(() {
+        widget.subcat.name = value;
+      });
+      SQLQueryClass.updateSubcategory(widget.subcat);
+    }
+  }
+
+  handleSubcategoryBudgetedChange() {
+    print("Changed budgeted value in subcategory");
+    if (_budgetedController.numberValue != widget.subcat.budgeted) {
+      setState(() {
+        widget.subcat.budgeted = _budgetedController.numberValue;
+      });
+      SQLQueryClass.updateSubcategory(widget.subcat);
+    }
+  }
+
+  handleSubcategoryAvailableChange() {
+    print("Changed available value in subcategory");
+    if (_budgetedController.numberValue != widget.subcat.available) {
+      setState(() {
+        widget.subcat.available = _availableController.numberValue;
+      });
+      SQLQueryClass.updateSubcategory(widget.subcat);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => _SubcategoryRowView(this);
+}
+
+class _SubcategoryRowView extends WidgetView<SubcategoryRow, _SubcategoryRowController> {
+  _SubcategoryRowView(_SubcategoryRowController state) : super(state);
+
+  final TextStyle _subcategoryTextStyle = new TextStyle(color: Colors.black, fontSize: 16.0);
+  final TextStyle _greenNumberTextStyle = new TextStyle(color: Colors.green, fontSize: 16.0);
+  final TextStyle _redNumberTextStyle = new TextStyle(color: Colors.red, fontSize: 16.0);
+
   @override
   Widget build(BuildContext context) {
     print(widget.subcat);
-    _budgetedController.updateValue(widget.subcat.budgeted);
-    _availableController.updateValue(widget.subcat.available);
+    state._budgetedController.updateValue(widget.subcat.budgeted);
+    state._availableController.updateValue(widget.subcat.available);
 
     return Container(
       padding: EdgeInsets.symmetric(vertical: 15),
@@ -59,71 +98,166 @@ class _SubcategoryRowState extends State<SubcategoryRow> {
             decoration: new InputDecoration.collapsed(
               hintText: "",
             ),
-            controller: _nameController,
+            controller: state._nameController,
             textAlign: TextAlign.left,
             inputFormatters: [LengthLimitingTextInputFormatter(25)], //To remove length counter
             textInputAction: TextInputAction.done,
             style: _subcategoryTextStyle,
-            onFieldSubmitted: (String value) {
-              print("Changed available value in subcategory");
-              if (_nameController.text != widget.subcat.name) {
-                setState(() {
-                  widget.subcat.name = _nameController.text;
-                  SQLQueryClass.updateSubcategory(widget.subcat);
-                });
-              }
-              ;
-            },
+            onFieldSubmitted: (String value) => state.handleSubcategoryNameChange(value),
           )),
           Expanded(
             child: TextFormField(
-              decoration: new InputDecoration.collapsed(
-                hintText: "",
-              ),
-              keyboardType: TextInputType.numberWithOptions(),
-              controller: _budgetedController,
-              inputFormatters: [LengthLimitingTextInputFormatter(12)], //To remove length counter
-              textInputAction: TextInputAction.done,
-              textAlign: TextAlign.right,
-              style: _subcategoryTextStyle,
-              // When the user presses the 'Enter' key, update the respective entry in the database
-              onFieldSubmitted: (String value) {
-                print("Changed budgeted value in subcategory");
-                if (_budgetedController.numberValue != widget.subcat.budgeted) {
-                  setState(() {
-                    widget.subcat.budgeted = _budgetedController.numberValue;
-                  });
-                  SQLQueryClass.updateSubcategory(widget.subcat);
-                }
-              },
-            ),
+                decoration: new InputDecoration.collapsed(
+                  hintText: "",
+                ),
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: state._budgetedController,
+                inputFormatters: [LengthLimitingTextInputFormatter(12)], //To remove length counter
+                textInputAction: TextInputAction.done,
+                textAlign: TextAlign.right,
+                style: _subcategoryTextStyle,
+                // When the user presses the 'Enter' key, update the respective entry in the database
+                onFieldSubmitted: (String value) => state.handleSubcategoryBudgetedChange()),
           ),
           Expanded(
             child: TextFormField(
               decoration: new InputDecoration.collapsed(hintText: "${widget.subcat.available}"),
               keyboardType: TextInputType.numberWithOptions(),
-              controller: _availableController,
+              controller: state._availableController,
               inputFormatters: [LengthLimitingTextInputFormatter(12)], //To remove length counter
               textInputAction: TextInputAction.done,
 
               textAlign: TextAlign.right,
               style: widget.subcat.available > 0 ? _greenNumberTextStyle : _redNumberTextStyle,
               // When the user presses the 'Enter' key, update the respective entry in the database
-              onFieldSubmitted: (String value) {
-                print("Changed available value in subcategory");
-                if (_budgetedController.numberValue != widget.subcat.available) {
-                  setState(() {
-                    widget.subcat.available = _availableController.numberValue;
-                  });
-                  SQLQueryClass.updateSubcategory(widget.subcat);
-                }
-              },
+              onFieldSubmitted: (String value) => state.handleSubcategoryAvailableChange(),
             ),
           )
         ],
       ),
     );
   }
-  //TODO: Big numbers get cutoff when sending to database (e.g. 50 0004.05 becommes 5000.0, small numbers too !)
-  //TODO: Fix numbers not loading from DB --> DB IS NOT THE CULPRIT? THE CONTROLLEr IS
 }
+
+// class SubcategoryRow extends StatefulWidget {
+//   final SubCategory subcat;
+//   SubcategoryRow({Key key, @required this.subcat}) : super(key: key);
+
+//   @override
+//   _SubcategoryRowState createState() => _SubcategoryRowState();
+// }
+
+// class _SubcategoryRowState extends State<SubcategoryRow> {
+//   MoneyMaskedTextController _budgetedController;
+//   MoneyMaskedTextController _availableController;
+//   TextEditingController _nameController;
+
+//   final TextStyle _subcategoryTextStyle = new TextStyle(color: Colors.black, fontSize: 16.0);
+//   final TextStyle _greenNumberTextStyle = new TextStyle(color: Colors.green, fontSize: 16.0);
+//   final TextStyle _redNumberTextStyle = new TextStyle(color: Colors.red, fontSize: 16.0);
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _nameController = new TextEditingController(text: '${widget.subcat.name}');
+
+//     _availableController = new MoneyMaskedTextController(
+//         decimalSeparator: '.', thousandSeparator: ' ', rightSymbol: ' \€');
+
+//     _budgetedController = new MoneyMaskedTextController(
+//         decimalSeparator: '.', thousandSeparator: ' ', rightSymbol: ' \€');
+//   }
+
+//   @override
+//   void dispose() {
+//     _availableController.dispose();
+//     _budgetedController.dispose();
+//     _nameController.dispose();
+//     super.dispose();
+//   }
+
+//   //TODO : Update MainCategory budget when subcategory budget is changed
+//   @override
+//   Widget build(BuildContext context) {
+//     print(widget.subcat);
+//     _budgetedController.updateValue(widget.subcat.budgeted);
+//     _availableController.updateValue(widget.subcat.available);
+
+//     return Container(
+//       padding: EdgeInsets.symmetric(vertical: 15),
+//       margin: EdgeInsets.symmetric(horizontal: 10),
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//               child: TextFormField(
+//             decoration: new InputDecoration.collapsed(
+//               hintText: "",
+//             ),
+//             controller: _nameController,
+//             textAlign: TextAlign.left,
+//             inputFormatters: [LengthLimitingTextInputFormatter(25)], //To remove length counter
+//             textInputAction: TextInputAction.done,
+//             style: _subcategoryTextStyle,
+//             onFieldSubmitted: (String value) {
+//               print("Changed available value in subcategory");
+//               if (_nameController.text != widget.subcat.name) {
+//                 setState(() {
+//                   widget.subcat.name = _nameController.text;
+//                   SQLQueryClass.updateSubcategory(widget.subcat);
+//                 });
+//               }
+//               ;
+//             },
+//           )),
+//           Expanded(
+//             child: TextFormField(
+//               decoration: new InputDecoration.collapsed(
+//                 hintText: "",
+//               ),
+//               keyboardType: TextInputType.numberWithOptions(),
+//               controller: _budgetedController,
+//               inputFormatters: [LengthLimitingTextInputFormatter(12)], //To remove length counter
+//               textInputAction: TextInputAction.done,
+//               textAlign: TextAlign.right,
+//               style: _subcategoryTextStyle,
+//               // When the user presses the 'Enter' key, update the respective entry in the database
+//               onFieldSubmitted: (String value) {
+//                 print("Changed budgeted value in subcategory");
+//                 if (_budgetedController.numberValue != widget.subcat.budgeted) {
+//                   setState(() {
+//                     widget.subcat.budgeted = _budgetedController.numberValue;
+//                   });
+//                   SQLQueryClass.updateSubcategory(widget.subcat);
+//                 }
+//               },
+//             ),
+//           ),
+//           Expanded(
+//             child: TextFormField(
+//               decoration: new InputDecoration.collapsed(hintText: "${widget.subcat.available}"),
+//               keyboardType: TextInputType.numberWithOptions(),
+//               controller: _availableController,
+//               inputFormatters: [LengthLimitingTextInputFormatter(12)], //To remove length counter
+//               textInputAction: TextInputAction.done,
+
+//               textAlign: TextAlign.right,
+//               style: widget.subcat.available > 0 ? _greenNumberTextStyle : _redNumberTextStyle,
+//               // When the user presses the 'Enter' key, update the respective entry in the database
+//               onFieldSubmitted: (String value) {
+//                 print("Changed available value in subcategory");
+//                 if (_budgetedController.numberValue != widget.subcat.available) {
+//                   setState(() {
+//                     widget.subcat.available = _availableController.numberValue;
+//                   });
+//                   SQLQueryClass.updateSubcategory(widget.subcat);
+//                 }
+//               },
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+//   //TODO: Big numbers get cutoff when sending to database (e.g. 50 0004.05 becommes 5000.0, small numbers too !)
+//   //TODO: Fix numbers not loading from DB --> DB IS NOT THE CULPRIT? THE CONTROLLEr IS
+// }
