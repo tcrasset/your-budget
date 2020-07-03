aadcabc75db0c9433ac9c1417cadde90f2fa3a3f
tcrasset <tomcrasset@gmail.com>
2020-07-03 12:56:36 +0200

    Use Provider app state instead of FutureBuilder

--- lib/screens/showTransactions/showTransactionsPage.dart
+++ lib/screens/showTransactions/showTransactionsPage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mybudget/appState.dart';
 
-import 'package:mybudget/models/SQLQueries.dart';
 import 'package:mybudget/models/entries.dart';
 import 'package:mybudget/components/widgetViewClasses.dart';
 import 'package:mybudget/screens/showTransactions/components/transaction.dart';
+import 'package:provider/provider.dart';
 
 class ShowTransactionPage extends StatefulWidget {
   final String title;
@@ -36,59 +37,32 @@ class _ShowTransactionPageView
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: new AppBar(
-          title: Text(widget.title),
-        ),
-        body: new Column(children: [
-          Padding(padding: EdgeInsets.only(top: 20.0)),
-          Expanded(
-              child: FutureBuilder(
-                  // Perform async operations before displaying the interface
-                  future: Future.wait([
-                    SQLQueryClass.getMoneyTransactions(),
-                    SQLQueryClass.getPayees(),
-                    SQLQueryClass.getAccounts(),
-                    SQLQueryClass.getSubCategories(),
-                  ]),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.none:
-                        return new Container();
-                        break;
-                      case ConnectionState.waiting:
-                        return new Center(child: new CircularProgressIndicator());
-                        break;
-                      case ConnectionState.active:
-                        return new Container();
-                        break;
-                      case ConnectionState.done:
-                        {
-                          if (snapshot.hasError) {
-                            return new Text('${snapshot.error}',
-                                style: TextStyle(color: Colors.red));
-                          } else {
-                            state.moneyTransactionList = snapshot.data[0];
-                            print(state.moneyTransactionList);
-                            return new Container(
-                                child: new ListView.separated(
-                              shrinkWrap: true,
-                              itemCount: state.moneyTransactionList.length,
-                              separatorBuilder: (BuildContext context, int index) =>
-                                  Divider(height: 1, color: Colors.black12),
-                              itemBuilder: (BuildContext context, int index) {
-                                return transaction(snapshot, index);
-                              },
-                            ));
-                          }
-                        }
-                        break;
-                      default:
-                        return new Container();
-                        break;
-                    }
-                  }))
-        ]));
+    return Consumer<AppState>(builder: (context, appState, child) {
+      if (appState.categories.isEmpty) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else {
+        return _TransactionList();
+      }
+    });
+  }
+}
+
+class _TransactionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = Provider.of<AppState>(context);
+
+    return new Container(
+        child: new ListView.separated(
+      shrinkWrap: true,
+      itemCount: appState.transactions.length,
+      separatorBuilder: (BuildContext context, int index) =>
+          Divider(height: 1, color: Colors.black12),
+      itemBuilder: (BuildContext context, int index) {
+        return transaction(appState.transactions[index], appState.categories);
+      },
+    ));
   }
 }
