9bb39c273fbd3abae766713e34fcf526ab7eb077
tcrasset <tomcrasset@gmail.com>
2020-07-08 20:00:45 +0200

    Add a waiting screen before showing the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:mybudget/screens/addTransaction/addTransaction.dart';
 
 import 'package:mybudget/screens/budget/budgetPage.dart';
 import 'package:mybudget/models/database_creator.dart';
+import 'package:mybudget/screens/waitingscreen.dart';
 import 'package:mybudget/screens/showTransactions/showTransactionsPage.dart';
 import 'package:provider/provider.dart';
 // import 'package:mybudget/searchPage.dart';
@@ -12,34 +13,46 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseCreator().initDatabase();
-  runApp(ChangeNotifierProvider(create: (context) => AppState(), child: MyBudget()));
+  runApp(MyBudget());
 }
 
 class MyBudget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(text: "Budget"),
-                Tab(text: "Add new Transaction"),
-                Tab(text: "Transactions")
+    AppState appState = AppState();
+    return ChangeNotifierProvider<AppState>.value(value: appState, child: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AppState appState = Provider.of<AppState>(context);
+    if (appState.allCategories.isNotEmpty) {
+      return MaterialApp(
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: [
+                  Tab(text: "Budget"),
+                  Tab(text: "Add new Transaction"),
+                  Tab(text: "Transactions")
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                BudgetPage(title: 'Bugdet Page'),
+                AddTransactionPage(),
+                ShowTransactionPage(title: "Transactions")
               ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              BudgetPage(title: 'Bugdet Page'),
-              AddTransactionPage(),
-              ShowTransactionPage(title: "Transactions")
-            ],
-          ),
         ),
-      ),
-    );
+      );
+    }
+    return WaitingScreen();
   }
 }
