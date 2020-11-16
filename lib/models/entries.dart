8b6c5e22db4951e66c53c2fc4498f2ed7fb9f6d5
Tom Crasset <tomcrasset@protonmail.com>
2020-11-16 21:28:32 +0100

    Add documentation for Payee, Money Transaction, and Account"

--- lib/models/entries.dart
+++ lib/models/entries.dart
@@ -1,23 +1,25 @@
+/// This file will contain the classes for the common objects used throughout the codebase, namely
+/// [Payee], [MoneyTransaction], and [Account].
 import 'package:your_budget/models/database_creator.dart';
 
+/// Class that defines an entity/person that either received money or gave money to the budget
+/// holder.
+/// It is defined using an unique [id] and a [name].
 class Payee {
+  /// Unique id of this.
   int id;
+
+  /// Name of this.
   String name;
 
+  /// Default constructor of [Payee]
   Payee(this.id, this.name);
 
-  /// Create a Payee from a json file
+  /// Constructor building a [Payee] from a [json] representation taken
+  /// from a database.
   Payee.fromJson(Map<String, dynamic> json) {
     this.id = json[DatabaseCreator.PAYEE_ID];
     this.name = json[DatabaseCreator.PAYEE_NAME];
-  }
-
-  /// Convert a Payee into a Map.
-  Map<String, dynamic> toMap() {
-    return {
-      DatabaseCreator.PAYEE_ID: id,
-      DatabaseCreator.PAYEE_NAME: name,
-    };
   }
 
   @override
@@ -26,19 +28,40 @@ class Payee {
   }
 }
 
+/// Class that defines a MoneyTransaction between the user and another entity.
+/// The [amount] is transfered between the user and the entity specified by [payeeID] at the given
+/// [date], from the account specified by [accountID] and the subcategory specified
+/// by [subcategoryID].
+/// A [memo] detailing the transaction is added as well.
+
 class MoneyTransaction {
+  /// Unique id of this.
   int id;
+
+  /// Id of the SubCategory that this is operating on.
   int subcatID;
+
+  /// Id of the entity where the money comes from/goes to.
   int payeeID;
+
+  /// Id of the account where the money is input/output.
   int accountID;
+
+  /// Monetary value that is exchanged.
   double amount;
+
+  /// Description of this.
   String memo;
+
+  /// Date of this.
   DateTime date;
 
+  /// Default constructor of [MoneyTransaction].
   MoneyTransaction(
       this.id, this.subcatID, this.payeeID, this.accountID, this.amount, this.memo, this.date);
 
-  /// Create a MoneyTransaction from a json file
+  /// Constructor building a [MoneyTransaction] from a [json] representation taken
+  /// from a database.
   MoneyTransaction.fromJson(Map<String, dynamic> json) {
     this.id = json[DatabaseCreator.MONEYTRANSACTION_ID];
     this.subcatID = json[DatabaseCreator.SUBCAT_ID_OUTSIDE];
@@ -49,19 +72,6 @@ class MoneyTransaction {
     this.date = DateTime.fromMillisecondsSinceEpoch(json[DatabaseCreator.MONEYTRANSACTION_DATE]);
   }
 
-  ///Convert a MoneyTransaction into a Map.
-  Map<String, dynamic> toMap() {
-    return {
-      DatabaseCreator.MONEYTRANSACTION_ID: id,
-      DatabaseCreator.SUBCAT_ID_OUTSIDE: subcatID,
-      DatabaseCreator.PAYEE_ID_OUTSIDE: payeeID,
-      DatabaseCreator.ACCOUNT_ID_OUTSIDE: accountID,
-      DatabaseCreator.MONEYTRANSACTION_AMOUNT: amount,
-      DatabaseCreator.MONEYTRANSACTION_MEMO: memo,
-      DatabaseCreator.MONEYTRANSACTION_DATE: date,
-    };
-  }
-
   @override
   String toString() {
     return super.toString() +
@@ -69,26 +79,27 @@ class MoneyTransaction {
   }
 }
 
+/// Class representing the physical account where the money is stored.
 class Account {
+  /// Unique id of this.
   int id;
+
+  /// Name given to this.
   String name;
+
+  /// Balance of this.
   double balance;
 
+  /// Default constructor for [Account] with an unique [id], a [name] and the
+  /// current [balance].
   Account(this.id, this.name, this.balance);
 
+  /// Constructor building an [Account] from a [json] representation taken
+  /// from a database.
   Account.fromJson(Map<String, dynamic> json) {
     this.id = json[DatabaseCreator.ACCOUNT_ID];
     this.name = json[DatabaseCreator.ACCOUNT_NAME];
     this.balance = json[DatabaseCreator.ACCOUNT_BALANCE];
-  }
-
-  /// Convert an Account into a Map.
-  Map<String, dynamic> toMap() {
-    return {
-      DatabaseCreator.ACCOUNT_ID: id,
-      DatabaseCreator.ACCOUNT_NAME: name,
-      DatabaseCreator.ACCOUNT_BALANCE: balance,
-    };
   }
 
   @override
