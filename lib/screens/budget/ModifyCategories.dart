02b757b80b14a51b9edb8452f901537426b32711
Tom Crasset <tomcrasset@protonmail.com>
2020-11-18 08:49:50 +0100

    Remove unused import

--- lib/screens/budget/ModifyCategories.dart
+++ lib/screens/budget/ModifyCategories.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:your_budget/appState.dart';
 import 'package:your_budget/models/categories.dart';
