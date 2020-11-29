319142a8e781c8a2dc9deb9653f23a7936bf246f
Tom Crasset <tomcrasset@protonmail.com>
2020-11-29 19:57:34 +0100

    Change 'categories' to 'transactions'

--- lib/screens/showTransactions/modifyTransactions.dart
+++ lib/screens/showTransactions/modifyTransactions.dart
@@ -66,7 +66,7 @@ class _ModifyTransactionsView
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Modify categories"),
+        title: Text("Modify transactions"),
       ),
       body: Consumer2<AppState, ShowTransactionsState>(
           builder: (_, appState, showTransactionsState, __) {
