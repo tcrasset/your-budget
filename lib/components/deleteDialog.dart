ffec5811fa75e9e61e6434b1b937134c200ada39
Tom Crasset <tomcrasset@protonmail.com>
2020-12-05 14:46:59 +0100

    Rename warningText using lowerCamelCase

--- lib/components/deleteDialog.dart
+++ lib/components/deleteDialog.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:your_budget/models/constants.dart';
 
-Future<String> showDeleteDialog(BuildContext context, String warning_text) async {
+Future<String> showDeleteDialog(BuildContext context, String warningText) async {
   return showDialog<String>(
     context: context,
     barrierDismissible: false, // user must tap button!
     builder: (BuildContext context) {
       return AlertDialog(
-        title: Text(warning_text),
+        title: Text(warningText),
         actions: <Widget>[
           FlatButton(
             child: Text('Cancel'),
