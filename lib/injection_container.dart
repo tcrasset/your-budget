ad3e84b5ee1c064e79aae4111bb757fedf90d385
Tom Crasset <tomcrasset@protonmail.com>
2020-12-19 17:20:48 +0100

    Inject SQLQueryClass into AppState
    #9

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:your_budget/appState.dart';
+import 'package:your_budget/models/SQLQueries.dart';
+import 'package:your_budget/models/queries.dart';
 
 final sl = GetIt.instance;
 
 
 
 void init(){
-
+  sl.registerFactory(() => AppState(queryContext: sl()));
+  sl.registerLazySingleton<Queries>(() => SQLQueryClass());
 }
