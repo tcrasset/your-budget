4f8050276a968424bb6a4d36bcfdd3c31858fe9c
Tom Crasset <tomcrasset@protonmail.com>
2021-01-02 11:51:00 +0100

    Append category name to duplicate SubCategories
    #26

--- lib/screens/addTransaction/selectValue.dart
+++ lib/screens/addTransaction/selectValue.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:your_budget/appState.dart';
 import 'package:your_budget/components/addDialog.dart';
+import 'package:your_budget/models/categories.dart';
 import 'package:your_budget/models/entries.dart';
 import 'package:provider/provider.dart';
 
@@ -17,6 +18,7 @@ class SelectValuePageState extends State<SelectValuePage> {
   TextEditingController searchController = new TextEditingController();
   String filter;
   bool isPayee;
+  bool isSubcategories;
 
   AppState appState;
   @override
@@ -24,6 +26,7 @@ class SelectValuePageState extends State<SelectValuePage> {
     super.initState();
     appState = Provider.of(context, listen: false);
     isPayee = widget.title == "Payees";
+    isSubcategories = widget.title == "Subcategories";
     searchController.addListener(() {
       setState(() {
         filter = searchController.text;
@@ -66,8 +69,70 @@ class SelectValuePageState extends State<SelectValuePage> {
     }
   }
 
+  List<dynamic> _getDuplicateEntries(){
+    var map = Map<String, int>(); // Mapping between name and id
+
+    Set<int> duplicateIDs = Set<int>();
+    widget.listEntries.forEach((element) {
+      if(element is SubCategory){
+        if(!map.containsKey(element.name)) {
+          map[element.name] = element.id;
+        } else {
+          duplicateIDs.add(element.id);
+          duplicateIDs.add(map[element.name]); //Other duplicate that is in map
+        }
+      }
+    });
+    return appState.subcategories.where((element) => duplicateIDs.contains(element.id)).toList();
+  }
+
+
+  List _addLabelForDuplicateEntries(List<dynamic> duplicateEntries) {
+
+    List modifiedListEntries = [];
+    List maincategories = appState.allCategories.whereType<MainCategory>().toList();
+
+    widget.listEntries.forEach((entry) {
+      if(entry is SubCategory){
+
+        bool isDuplicate = duplicateEntries.singleWhere(
+          (duplicate) => duplicate.id == entry.id, orElse: () => null) != null;
+
+        if(isDuplicate){
+          var modifiedEntry = _addCategoryName(entry, maincategories);
+          modifiedListEntries.add(modifiedEntry);
+        }else{
+          modifiedListEntries.add(entry);
+        }
+      }
+    });
+    return modifiedListEntries;
+  }
+
+  dynamic _addCategoryName(var entry, List maincategories) {
+    var modifiedEntry = entry.copy();
+    MainCategory category = maincategories.singleWhere((maincat) => maincat.id == entry.parentId, orElse: null);
+    modifiedEntry.name = modifiedEntry.name + ' (' + category.name + ')' ?? "" ;
+    return modifiedEntry;
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    List listEntries = [];
+    if(isSubcategories){
+
+      List<dynamic> duplicates = _getDuplicateEntries();
+      if(duplicates.isNotEmpty)
+        listEntries = _addLabelForDuplicateEntries(duplicates);
+
+      else
+        listEntries = widget.listEntries;
+    }
+    else
+      listEntries = widget.listEntries;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -101,20 +166,21 @@ class SelectValuePageState extends State<SelectValuePage> {
           Expanded(
               child: ListView.separated(
             shrinkWrap: true,
-            itemCount: widget.listEntries.length,
+            itemCount: listEntries.length,
             separatorBuilder: (BuildContext context, int index) =>
                 Divider(height: 1, color: Colors.black12),
             itemBuilder: (BuildContext context, int index) {
-              var item = widget.listEntries[index];
-              print(item);
-              bool noFilter = filter == null || filter == "";
+
+              var item = listEntries[index];
               var itemToShow = item is Text ? item : Text(item.name);
               var itemToFilter = item is Text ? item.data : item.name;
+              bool noFilter = filter == null || filter == "";
+
               if (noFilter == true)
                 // The filter is empty, show everything
                 return ListTile(
                     title: itemToShow, onTap: () => handlePopContext(item));
-              else if (noFilter = false) {
+              else if (noFilter == false) {
                 // The filter is not empty, we filter by name
                 if (itemToFilter.toLowerCase().contains(filter.toLowerCase()))
                   return ListTile(
@@ -128,4 +194,5 @@ class SelectValuePageState extends State<SelectValuePage> {
       ),
     );
   }
+
 }
