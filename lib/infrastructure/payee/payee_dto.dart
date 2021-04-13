afd489ecda364fecb33cde1694fc7b6f94e5c66c
Tom Crasset <tomcrasset@protonmail.com>
2021-04-13 19:57:08 +0200

    Create Payee DTO

---
+++ lib/infrastructure/payee/payee_dto.dart
@@ -0,0 +1,39 @@
+// Package imports:
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+// Project imports:
+import 'package:your_budget/domain/core/name.dart';
+import 'package:your_budget/domain/core/unique_id.dart';
+import 'package:your_budget/domain/payee/payee.dart';
+
+part 'payee_dto.freezed.dart';
+part 'payee_dto.g.dart';
+
+@freezed
+abstract class PayeeDTO implements _$PayeeDTO {
+  const PayeeDTO._();
+
+  const factory PayeeDTO({
+    @JsonKey(ignore: true) String id, //Do not use id in database
+    @required String name,
+  }) = _PayeeDTO;
+
+  factory PayeeDTO.fromDomain(Payee payee) {
+    return PayeeDTO(
+      id: payee.id.getOrCrash(), //Not used in database
+      name: payee.name.getOrCrash(),
+    );
+  }
+
+  Payee toDomain() {
+    return Payee(
+      id: UniqueId.fromUniqueString(id),
+      name: Name(name),
+    );
+  }
+
+  factory PayeeDTO.fromJson(Map<String, dynamic> json) => _$PayeeDTOFromJson(json);
+  factory PayeeDTO.fromSQL(Map<String, dynamic> json) {
+    return PayeeDTO.fromJson(json).copyWith(id: json["id"].toString());
+  }
+}
---
+++ lib/infrastructure/payee/payee_dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payee_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_PayeeDTO _$_$_PayeeDTOFromJson(Map<String, dynamic> json) {
+  return _$_PayeeDTO(
+    name: json['name'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_PayeeDTOToJson(_$_PayeeDTO instance) => <String, dynamic>{
+      'name': instance.name,
+    };
