2d9d5552f7f33c7a23a1023c8b3b12643ae0774e
Tom Crasset <tomcrasset@protonmail.com>
2021-04-17 16:56:43 +0200

    Create Subcategory value object

---
+++ lib/domain/subcategory/subcategory.dart
@@ -0,0 +1,45 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+// Project imports:
+import 'package:your_budget/domain/core/amount.dart';
+import 'package:your_budget/domain/core/name.dart';
+import 'package:your_budget/domain/core/unique_id.dart';
+import 'package:your_budget/domain/core/value_failure.dart';
+
+part 'subcategory.freezed.dart';
+
+/// Class that defines an entity/person that either received money or gave money to the budget
+/// holder.
+/// It is defined using an unique [id] and a [name].
+@freezed
+abstract class Subcategory implements _$Subcategory {
+  const factory Subcategory({
+    @required UniqueId id,
+    @required UniqueId parentID,
+    @required Name name,
+    @required Amount budgeted,
+    @required Amount available,
+  }) = _Subcategory;
+
+  const Subcategory._();
+
+  factory Subcategory.empty() => Subcategory(
+        id: UniqueId(),
+        parentID: UniqueId(),
+        name: Name(""),
+        budgeted: Amount(""),
+        available: Amount(""),
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return name.failureOrUnit.andThen(budgeted.failureOrUnit).andThen(available.failureOrUnit).fold(
+          (f) => some(f),
+          (_) => none(),
+        );
+  }
+}
